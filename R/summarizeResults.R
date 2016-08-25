0c4999026b4ec677e3976d197b64e1830cd17b7e
Alexey Shiklomanov <ashiklom@bu.edu>
2016-08-25 15:55:15 -0400

    More bugfixes to summarizeResults et al.

--- R/summarizeResults.R
+++ R/summarizeResults.R
@@ -5,14 +5,23 @@ summary_stats <- function(x) {
 }
 
 summarizeSigma <- function(samples, pft_name){
-    apply(samples, 2, summary_stats) %>%
-    t %>%
-    "rownames<-"(gsub("Sigma_.*\\.(log\\..*\\.log\\..*)", 
-                      "\\1", rownames(.))) %>%
-    .[apply(trait.combine, 2, paste, collapse = "."),] %>%
-    as.data.table(keep.rownames = TRUE) %>%
-    setnames("rn", "trait") %>%
-    .[, PFT := pft_name]
+    out <- apply(samples, 2, summary_stats) %>%
+        t %>%
+        "rownames<-"(gsub("Sigma_.*\\.(log\\..*\\.log\\..*)", 
+                          "\\1", rownames(.))) %>%
+        .[apply(trait.combine, 2, paste, collapse = "."),] %>%
+        as.data.table(keep.rownames = TRUE) %>%
+        setnames("rn", "trait") %>%
+        .[, PFT := pft_name] %>%
+        separate(PFT, into=c("Biome", "Function"), sep="_", 
+                 extra="merge", remove=FALSE) %>%
+        separate(Function, into = c("growth_form", "ps_type", 
+                                    "leaf_type", "phenology"),
+                 sep = "_", extra = "drop", remove=FALSE) %>%
+        setDT()
+    out[is.na(ps_type), 
+        c("ps_type", "leaf_type", "phenology") := Function]
+    return(out)
 }
 
 summarizeSigmaPFT <- function(dat, Cor = FALSE) {
@@ -23,14 +32,6 @@ summarizeSigmaPFT <- function(dat, Cor = FALSE) {
         dat.summary <- summarizeSigma(dat.pft, pft_name)
         dat.list[[pft_name]] <- dat.summary
     }
-    out <- rbindlist(dat.list) %>%
-        separate(PFT, into=c("Biome", "Function"), sep="_", 
-                 extra="merge", remove=FALSE) %>%
-        separate(Function, into = c("growth_form", "ps_type", 
-                                    "leaf_type", "phenology"),
-                sep = "_", extra = "drop", remove=FALSE) %>%
-        setDT()
-    out[is.na(ps_type), 
-        c("ps_type", "leaf_type", "phenology") := Function]
+    out <- rbindlist(dat.list)
 return(out)
 }
