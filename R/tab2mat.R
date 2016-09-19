b3dca75417032b2d3bcfbb3435c7c57c21660202
Alexey Shiklomanov <ashiklom@bu.edu>
2016-09-19 13:16:59 -0400

    Minor updates to figure code

--- R/tab2mat.R
+++ R/tab2mat.R
@@ -3,6 +3,7 @@ tab2mat <- function(dat, colname = "Mean", ...) {
     library(data.table)
     stopifnot(is.data.table(dat))
     vec <- dat[[colname]]
+    names(vec) <- dat[["trait"]]
     mat <- lowerdiag2mat(vec, ...)
     return(mat)
 }
@@ -14,9 +15,16 @@ lowerdiag2mat <- function(vec, corr = FALSE) {
     nmat <- 0.5 * (sqrt(8 * nvec + 1) - 1)
     stopifnot(nmat %% 1 == 0)
     if (corr) nmat <- nmat + 1
+    namesvec <- names(vec)
+    splitnames <- strsplit(namesvec, split="\\.")
+    allpars <- unique(unlist(splitnames))
+    stopifnot(length(allpars) == nmat)
     mat <- matrix(numeric(), nmat, nmat)
-    mat[lower.tri(mat, diag=!corr)] <- vec
-    mat[upper.tri(mat)] <- mat[lower.tri(mat)]
+    rownames(mat) <- colnames(mat) <- allpars
+    for (i in seq_along(splitnames)) {
+        mat[splitnames[[i]][1],splitnames[[i]][2]] <- vec[namesvec[i]]
+        mat[splitnames[[i]][2],splitnames[[i]][1]] <- vec[namesvec[i]]
+    }
     if (corr) diag(mat) <- 1
     return(mat)
 }
