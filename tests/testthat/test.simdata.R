ebcf401f4d71c4b8776450167dd9dbae2e3871ac
Alexey Shiklomanov <ashiklom@bu.edu>
2017-06-15 13:02:17 -0400

    Clean up tests

--- tests/testthat/test.simdata.R
+++ tests/testthat/test.simdata.R
@@ -1,22 +1,22 @@
 rm(list = ls())
 
 library(testthat)
-library(mvtraits)
-#devtools::load_all('.')
-library(mvtnorm)
-library(clusterGeneration)
+if (interactive()) {
+    devtools::load_all('.')
+} else {
+    library(mvtraits)
+}
 
 # Simulate some data
 mu <- c(10, 5, 0, -5, 10)
-Sig <- genPositiveDefMat(length(mu))$Sigma
+Sig <- clusterGeneration::genPositiveDefMat(length(mu))$Sigma
 
 N <- 1000
 ngroup <- 7
-dat_all <- rmvnorm(N, mu, Sig)
+dat_all <- mvtnorm::rmvnorm(N, mu, Sig)
 groups <- sample.int(ngroup, N, replace = TRUE)
 
-# Randomly remove some data
-#dat <- dat_all[groups == 1,]
+# Randomly remove half of the data
 dat <- dat_all
 
 nmiss <- round(length(dat) * 0.5)
@@ -26,24 +26,8 @@ dat[miss] <- NA
 
 custom_inputs <- list()
 
-#dat[dat[,"pft"] == 1, "pft"] <- 2
-#dat[1, "pft"] <- 1
-#dat <- as.data.table(dat)
 dir.create("output", showWarnings = FALSE)
 
 fit_uni <- runModel('uni', dat[groups == 1,])
 fit_multi <- runModel('multi', dat[groups == 1,])
-fit_hier <- runModel('hier', dat, groups = groups, iter = 100)
-#fit_uni <- runModel("uni", dat, NA)
-#saveRDS(fit_uni, "output/uni.rds")
-#fit_uni_1 <- runModel("uni", dat, 1)
-#saveRDS(fit_uni_1, "output/uni.1.rds")
-#fit_multi <- runModel("multi", dat, NA)
-#saveRDS(fit_multi, "output/multi.rds")
-#for (i in seq_len(npft)) {
-    #message("Running pft ", i)
-    #fit_multi_pft <- runModel("multi", dat, i)
-#}
-#saveRDS(fit_multi_1, "output/multi.1.rds")
-#fit_hier <- runModel("hier", dat, NA)
-#saveRDS(fit_hier, "output/hier.rds")
+fit_hier <- runModel('hier', dat, groups = groups, iter = 100, max.attempts = 1)
