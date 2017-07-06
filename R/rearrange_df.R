a93cc3a80bc54046fe5379cea1bc79832f5af804
Alexey Shiklomanov <ashiklom@bu.edu>
2017-07-06 10:28:33 -0400

    Functions for re-ordering data frame
    Also, minor revisions to grid plot.
    
    Resolves #1.

---
+++ R/rearrange_df.R
@@ -0,0 +1,81 @@
+#' Rearrange result data frame into correct parameter order
+#' 
+#' @param dat_full Summary tibble
+#' @param params Vector of params, in correct order
+#' @export
+rearrange_df <- function(dat_full, params) {
+    nparams <- length(params)
+    dat_bignest <- tidyr::nest(dat_full, -variable)
+    dat_bigproc <- dplyr::mutate(dat_bignest, 
+                                 data = purrr::map2(data, variable, ~procdat(.x, params, .y)))
+    dat_unnest <- tidyr::unnest(dat_bigproc)
+    return(dat_unnest)
+}
+
+procdat <- function(dat, params, variable) {
+    if (variable == 'mu') {
+        out <- proc_vec(dat, params)
+    } else if (variable %in% c('Sigma', 'Corr')) {
+        out2 <- proc_mat(dat, params)
+        if (variable == 'Corr') {
+            out <- filter(out2, as.character(xparam) != as.character(yparam))
+        } else {
+            out <- out2
+        }
+    }
+    return(out)
+}
+
+proc_vec <- function(dat, params) {
+    dat %>% 
+        dplyr::mutate(param = factor(index, levels = params)) %>% 
+        dplyr::select(-index)
+}
+
+proc_mat <- function(dat, params) {
+    oldnames <- names(dat)
+    newnames <- make.names(oldnames)
+    colnames(dat) <- newnames
+    value_cols <- oldnames[!oldnames %in% c('group', 'index')]
+    dat_nested <- tidyr::nest(dat, -group)
+    out_nested <- dplyr::mutate(dat_nested, 
+                                data = purrr:::map(data, reorder_df, 
+                                                   params = params, value_cols = value_cols))
+    out <- tidyr::unnest(out_nested)
+    return(out)
+}
+
+reorder_df <- function(dat, params, value_cols) {
+    dat_split <- tidyr::separate(dat, index, c('xvar', 'yvar'), sep = varsep_esc)
+    out <- dat_split %>% 
+        dplyr::select_if(is.double) %>% 
+        lapply(., dfcol2df, xvar = dat_split[['xvar']], yvar = dat_split[['yvar']], params) %>% 
+        Reduce(f = function(x, y) left_join(x, y, by = c('xparam', 'yparam')), x = .)
+    colnames(out)[grep('value', colnames(out))] <- value_cols
+    return(out)
+}
+
+dfcol2df <- function(column, xvar, yvar, params) {
+    mat <- dfcol2mat(column, xvar, yvar, params)
+    vec <- mvtraits:::flatten_matrix(mat)
+    new_vars <- strsplit(names(vec), split = varsep_esc)
+    xparam <- sapply(new_vars, '[[', 1) %>% factor(levels = params[-1])
+    yparam <- sapply(new_vars, '[[', 2) %>% factor(levels = params[-length(params)])
+    out <- tibble::tibble(xparam = xparam, yparam = yparam, value = vec)
+    return(out)
+}
+
+dfcol2mat <- function(column, xvar, yvar, params) {
+    m <- length(params)
+    n <- length(column)
+    mat <- diag(1, m)
+    dimnames(mat) <- list(params, params)
+    for (i in seq_len(n)) {
+        xx <- xvar[i]
+        yy <- yvar[i]
+        mat[xx, yy] <- column[i]
+        if (xx != yy) mat[yy, xx] <- column[i]
+    }
+    return(mat)
+}
+
--- scripts/read_summaries.R
+++ scripts/read_summaries.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(mvtraits)
+if (interactive()) devtools::install()
 
 raw_mass <- readRDS('summaries/mass_hier.rds')
 raw_area <- readRDS('summaries/area_hier.rds')
@@ -7,40 +9,33 @@ try_data <- readRDS('~/Projects/new-phytologist-traits/preprocess-try/traits_ana
 pft_levels <- try_data %>% pull(pft) %>% levels() %>% c('global', .)
 group_names <- raw_mass %>% distinct(group) %>% pull(group) %>% sort()
 
-proc <- . %>%
-    mutate(pft = factor(group, levels = group_names) %>% 'levels<-'(pft_levels)) %>% 
-    separate(index, c('xvar', 'yvar'), sep = '\\.\\.', extra = 'merge')
+proc <- function(dat, params) {
+    dat %>% 
+        select(-rowname) %>% 
+        mutate(index = gsub('group..\\.\\.', '', index)) %>% 
+        rearrange_df(params) %>% 
+        mutate(pft = factor(group, group_names) %>% 'levels<-'(pft_levels))
+}
 
-mass_proc <- raw_mass %>% proc
-area_proc <- raw_area %>% proc %>% 
-    mutate(yvar = gsub('group..\\.\\.', '', yvar))
+mass_params <- c('leaf_lifespan', 'SLA', 'Nmass', 'Pmass', 'Rdmass', 'Vcmax_mass', 'Jmax_mass')
+area_params <- gsub('mass$', 'area', mass_params)
+
+proc_mass <- proc(raw_mass, mass_params)
+proc_area <- proc(raw_area, area_params)
 
 # Assign PFT colors
 pft_colors <- c('black', RColorBrewer::brewer.pal(length(pft_levels) - 1, 'Paired'))
 names(pft_colors) <- pft_levels
 
-grid_corr_plot <- function(dat) {
-    ggplot(dat %>% filter(variable == 'Corr')) +
-        aes(x = pft, y = Mean, ymin = `2.5%`, ymax = `97.5%`, fill = pft) + 
-        geom_col() +
-        geom_errorbar(width = 0.2) +
-        facet_wrap(~xvar + yvar, drop = FALSE) + 
-        scale_fill_manual(values = pft_colors) + 
-        theme(axis.text.x = element_blank())
+grid_corr_plot <- function(dat, ...) {
+    summary_barplot(dat, 'Corr', ...) + scale_fill_manual(values = pft_colors)
 }
 
 fig_dir <- file.path('figures', 'gibbs')
 dir.create(fig_dir, showWarnings = FALSE)
 
-mass_plt <- grid_corr_plot(mass_proc)
+(mass_plt <- grid_corr_plot(proc_mass))
 ggsave(filename = file.path(fig_dir, 'mass_corrmat.pdf'), plot = mass_plt, width = 10, height = 8)
-area_plt <- grid_corr_plot(area_proc)
-ggsave(filename = file.path(fig_dir, 'area_corrmat.pdf'), plot = area_plt, width = 10, height = 8)
 
-mass_proc %>% 
-    filter(pft == 'global', variable == 'Sigma') %>% 
-    ggplot() + 
-        aes(x = 1, y = Mean, ymin = `2.5%`, ymax = `97.5%`) + 
-        geom_col() + 
-        geom_errorbar(width = 0.2) +
-        facet_wrap(~xvar + yvar, drop = FALSE)
+(area_plt <- grid_corr_plot(proc_area, facet_list = list(facets = ~yparam + xparam)))
+ggsave(filename = file.path(fig_dir, 'area_corrmat.pdf'), plot = area_plt, width = 10, height = 8)
