bdd04fad5cffcfe9f3b763818a456cdcf9765f2d
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2018-09-12 13:36:56 -0400

    Fix Ziggurat object initialization in C++ header

--- src/mvtraits_zigg.h
+++ src/mvtraits_zigg.h
@@ -1,6 +1,9 @@
 #ifndef _ZIGG_
 #define _ZIGG_
 #include "mvtraits_common.h"
+
+// [[Rcpp::depends(RcppZiggurat)]]
+
 #include <Ziggurat.h>
-extern Ziggurat::Ziggurat::Ziggurat zigg;
+static Ziggurat::Ziggurat::Ziggurat zigg;
 #endif
