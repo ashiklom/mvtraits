59af586bfeda143992e73a646df11dc12f975891
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-08-21 15:51:57 -0400

    Style cleanup for fit_mvnorm_hier

--- R/fit_mvnorm_hier.R
+++ R/fit_mvnorm_hier.R
@@ -5,9 +5,18 @@
 #' @inheritParams fit_mvnorm
 #' @inherit fit_mvnorm return
 #' @export
-fit_mvnorm_hier <- function(dat, groups, niter = 5000, priors = list(), inits = list(), nchains = 3,
-                            autofit = FALSE, max_attempts = 10, keep_samples = Inf, threshold = 1.15,
-                            save_progress = NULL, progress = FALSE) {
+fit_mvnorm_hier <- function(dat,
+                            groups,
+                            niter = 5000,
+                            priors = list(),
+                            inits = list(),
+                            nchains = 3,
+                            autofit = FALSE,
+                            max_attempts = 10,
+                            keep_samples = Inf,
+                            threshold = 1.15,
+                            save_progress = NULL,
+                            progress = FALSE) {
 
   stopifnot(is.matrix(dat), length(groups) == nrow(dat))
 
@@ -16,7 +25,7 @@ fit_mvnorm_hier <- function(dat, groups, niter = 5000, priors = list(), inits = 
   nparam <- ncol(dat)
   param_names <- colnames(dat)
   if (is.null(param_names)) {
-    param_names <- sprintf('par%02d', seq_len(nparam))
+    param_names <- sprintf("par%02d", seq_len(nparam))
   }
 
   ngroup <- length(unique(groups))
@@ -26,13 +35,15 @@ fit_mvnorm_hier <- function(dat, groups, niter = 5000, priors = list(), inits = 
   if (is.factor(groups)) {
     group_names <- levels(groups)
   } else {
-    group_names <- sprintf('group%02d', seq_len(ngroup))
+    group_names <- sprintf("group%02d", seq_len(ngroup))
   }
   igroups <- as.integer(groups)
   ugroups <- sort(unique(igroups))
 
-  ndat <- nrow(dat)
-  setup_bygroup <- lapply(ugroups, function(x) setup_missing(dat[igroups == x,]))
+  setup_bygroup <- lapply(
+    ugroups,
+    function(x) setup_missing(dat[igroups == x,])
+  )
 
   # Where missing, use default priors
   default_priors <- gibbs_default_priors(nparam, ngroup)
@@ -43,19 +54,23 @@ fit_mvnorm_hier <- function(dat, groups, niter = 5000, priors = list(), inits = 
   }
 
   # Set priors in environment
-  mu0_global <- priors[['mu_global']]
-  Sigma0_global <- priors[['Sigma_global']]
-  v0_global <- priors[['v_global']]
-  S0_global <- priors[['S_global']]
+  mu0_global <- priors[["mu_global"]]
+  Sigma0_global <- priors[["Sigma_global"]]
+  v0_global <- priors[["v_global"]]
+  S0_global <- priors[["S_global"]]
 
-  mu0_group <- priors[['mu_group']]
-  Sigma0_group <- priors[['Sigma_group']]
-  v0_group <- priors[['v_group']]
-  S0_group <- priors[['S_group']]
+  mu0_group <- priors[["mu_group"]]
+  Sigma0_group <- priors[["Sigma_group"]]
+  v0_group <- priors[["v_group"]]
+  S0_group <- priors[["S_group"]]
 
   # Precalculate certain quantities
   Sigma0_global_inv <- solve(Sigma0_global)
-  Sigma0_group_inv <- vapply(seq_len(ngroup), function(x) solve(Sigma0_group[x,,]), Sigma0_global_inv)
+  Sigma0_group_inv <- vapply(
+    seq_len(ngroup),
+    function(x) solve(Sigma0_group[x,,]),
+    Sigma0_global_inv
+  )
   Sigma0_group_inv <- aperm(Sigma0_group_inv, c(3, 1, 2))
 
   # Draw initial conditions from priors
@@ -64,9 +79,10 @@ fit_mvnorm_hier <- function(dat, groups, niter = 5000, priors = list(), inits = 
   mu_group <- list()
   Sigma_group <- list()
   for (n in chainseq) {
-    mu_global[[n]] <- random_mvnorm(1, mu0_global, Sigma0_global)[1,]
+    mu_global[[n]] <- random_mvnorm(1, mu0_global, Sigma0_global)[1, ]
     names(mu_global[[n]]) <- param_names
-    Sigma_global[[n]] <- solve(rWishart(1, v0_global + nparam + 1, S0_global)[,,1])
+    Sigma_global[[n]] <- solve(rWishart(1, v0_global + nparam + 1,
+                                        S0_global)[,,1])
     dimnames(Sigma_global[[n]]) <- list(param_names, param_names)
 
     mu_group[[n]] <- matrix(NA_real_, nrow = ngroup, ncol = nparam)
@@ -74,9 +90,9 @@ fit_mvnorm_hier <- function(dat, groups, niter = 5000, priors = list(), inits = 
     Sigma_group[[n]] <- array(NA_real_, c(ngroup, nparam, nparam))
     dimnames(Sigma_group[[n]]) <- list(group_names, param_names, param_names)
     for (i in seq_len(ngroup)) {
-      mu_group[[n]][i,] <- random_mvnorm(1, mu0_group[i,], Sigma0_group[i,,])
+      mu_group[[n]][i, ] <- random_mvnorm(1, mu0_group[i,], Sigma0_group[i,,])
       #Sigma_group[[n]][i,,] <- solve(rWishart(1, v0_group[i] + nparam + 1, S0_group[i,,])[,,1])
-      Sigma_group[[n]][i,,] <- diag(1, nparam)
+      Sigma_group[[n]][i, , ] <- diag(1, nparam)
     }
   }
   default_inits <- list(mu_global = mu_global,
@@ -119,7 +135,7 @@ fit_mvnorm_hier <- function(dat, groups, niter = 5000, priors = list(), inits = 
   message("Running sampler...")
   raw_samples <- run_until_converged(
     sampler = sampler,
-    model_type = 'hier',
+    model_type = "hier",
     inits = inits,
     nchains = nchains,
     max_attempts = max_attempts,
@@ -130,7 +146,8 @@ fit_mvnorm_hier <- function(dat, groups, niter = 5000, priors = list(), inits = 
   )
 
   message("Calculating correlation matrices...")
-  raw_samples_corr <- add_correlations(raw_samples, hier = TRUE, ngroups = ngroup)
+  raw_samples_corr <- add_correlations(raw_samples,
+                                       hier = TRUE, ngroups = ngroup)
 
   message("Converting samples to coda mcmc.list object...")
   samples_mcmc <- results2mcmclist(raw_samples_corr, type = "hier")
@@ -138,26 +155,32 @@ fit_mvnorm_hier <- function(dat, groups, niter = 5000, priors = list(), inits = 
   niter <- coda::niter(samples_mcmc)
 
   message("Preparing summary table...")
-  summary_table <- summary_df(window(samples_mcmc, start = floor(niter / 2)), group = TRUE)
+  summary_table <- summary_df(
+    window(samples_mcmc, start = floor(niter / 2)),
+    group = TRUE
+  )
 
   stats <- c("Mean", "2.5%", "97.5%")
   mu_global_stats <- sapply(
     stats,
-    function(x) summary2vec(summary_table, x, variable == "mu", group == "global"),
+    function(x) summary2vec(summary_table, x,
+                            variable == "mu", group == "global"),
     simplify = FALSE,
     USE.NAMES = TRUE
   )
 
   Sigma_global_stats <- sapply(
     stats,
-    function(x) summary2mat(summary_table, x, variable == "Sigma", group == "global"),
+    function(x) summary2mat(summary_table, x,
+                            variable == "Sigma", group == "global"),
     simplify = FALSE,
     USE.NAMES = TRUE
   )
 
   Corr_global_stats <- sapply(
     stats,
-    function(x) summary2mat(summary_table, x, variable == "Corr", group == "global"),
+    function(x) summary2mat(summary_table, x,
+                            variable == "Corr", group == "global"),
     simplify = FALSE,
     USE.NAMES = TRUE
   )
@@ -180,9 +203,26 @@ fit_mvnorm_hier <- function(dat, groups, niter = 5000, priors = list(), inits = 
     )
   }
 
-  mu_group_stats <- sapply(group_names, get_mu_group, simplify = FALSE, USE.NAMES = TRUE)
-  Sigma_group_stats <- sapply(group_names, get_mat_group, var = "Sigma", simplify = FALSE, USE.NAMES = TRUE)
-  Corr_group_stats <- sapply(group_names, get_mat_group, var = "Corr", simplify = FALSE, USE.NAMES = TRUE)
+  mu_group_stats <- sapply(
+    group_names,
+    get_mu_group,
+    simplify = FALSE,
+    USE.NAMES = TRUE
+  )
+  Sigma_group_stats <- sapply(
+    group_names,
+    get_mat_group,
+    var = "Sigma",
+    simplify = FALSE,
+    USE.NAMES = TRUE
+  )
+  Corr_group_stats <- sapply(
+    group_names,
+    get_mat_group,
+    var = "Corr",
+    simplify = FALSE,
+    USE.NAMES = TRUE
+  )
 
   list(
     summary_table = summary_table,
